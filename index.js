dcc10cc5aa255205294e5f2626aa0065d35e8540
Brad Hambrice <hambrice@uab.edu>
2017-11-17 00:53:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,7 @@ function logWhisper(string) {
   console.log(whisper(string))
 }
 logWhisper('HELLO')
+function sayHiToGrandma(string) {
+  if string === string.toLowerCase()
+  return "I can't hear you!"
+}
